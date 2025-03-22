bcb37270e7119d6377b712954979c21e9c2a5b70
Bagdadyan_G <lpsneka@email.com>
2025-03-23 00:00:57 +0300

    Добавилa vite.config.js для G

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Если репозиторий не привязан к кастомному домену, оставь "./"
+  base: "./", 
   build: {
     outDir: "dist",
   },
